737fa9d8127ce423dd0a4d95942812bc164efc1f
wooorm <tituswormer@gmail.com>
2015-01-31 15:11:22 +0100

    Add `emoji-emotion.js`, `emoji-emotion.min.js`

---
+++ emoji-emotion.js
@@ -0,0 +1,283 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.emojiEmotion=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports=[
+  {
+    "emoji": "😠",
+    "polarity": -4
+  },
+  {
+    "emoji": "😧",
+    "polarity": -4
+  },
+  {
+    "emoji": "😲",
+    "polarity": 3
+  },
+  {
+    "emoji": "😊",
+    "polarity": 3
+  },
+  {
+    "emoji": "😰",
+    "polarity": -2
+  },
+  {
+    "emoji": "😖",
+    "polarity": -2
+  },
+  {
+    "emoji": "😕",
+    "polarity": -2
+  },
+  {
+    "emoji": "😢",
+    "polarity": -2
+  },
+  {
+    "emoji": "😿",
+    "polarity": -2
+  },
+  {
+    "emoji": "😞",
+    "polarity": -2
+  },
+  {
+    "emoji": "😥",
+    "polarity": -1
+  },
+  {
+    "emoji": "😵",
+    "polarity": -1
+  },
+  {
+    "emoji": "😑",
+    "polarity": 0
+  },
+  {
+    "emoji": "😨",
+    "polarity": -2
+  },
+  {
+    "emoji": "😳",
+    "polarity": -2
+  },
+  {
+    "emoji": "😦",
+    "polarity": -1
+  },
+  {
+    "emoji": "😬",
+    "polarity": -2
+  },
+  {
+    "emoji": "😁",
+    "polarity": -1
+  },
+  {
+    "emoji": "😀",
+    "polarity": 3
+  },
+  {
+    "emoji": "😍",
+    "polarity": 4
+  },
+  {
+    "emoji": "😻",
+    "polarity": 4
+  },
+  {
+    "emoji": "😯",
+    "polarity": -1
+  },
+  {
+    "emoji": "👿",
+    "polarity": -5
+  },
+  {
+    "emoji": "😇",
+    "polarity": 4
+  },
+  {
+    "emoji": "😂",
+    "polarity": 4
+  },
+  {
+    "emoji": "😹",
+    "polarity": 4
+  },
+  {
+    "emoji": "😗",
+    "polarity": 3
+  },
+  {
+    "emoji": "😽",
+    "polarity": 3
+  },
+  {
+    "emoji": "😚",
+    "polarity": 3
+  },
+  {
+    "emoji": "😘",
+    "polarity": 4
+  },
+  {
+    "emoji": "😙",
+    "polarity": 3
+  },
+  {
+    "emoji": "😆",
+    "polarity": 1
+  },
+  {
+    "emoji": "😷",
+    "polarity": -1
+  },
+  {
+    "emoji": "😐",
+    "polarity": 0
+  },
+  {
+    "emoji": "😶",
+    "polarity": 0
+  },
+  {
+    "emoji": "😮",
+    "polarity": -2
+  },
+  {
+    "emoji": "😔",
+    "polarity": -1
+  },
+  {
+    "emoji": "😣",
+    "polarity": -2
+  },
+  {
+    "emoji": "😾",
+    "polarity": -5
+  },
+  {
+    "emoji": "😡",
+    "polarity": -5
+  },
+  {
+    "emoji": "☺️",
+    "polarity": 3
+  },
+  {
+    "emoji": "😌",
+    "polarity": 3
+  },
+  {
+    "emoji": "😱",
+    "polarity": -4
+  },
+  {
+    "emoji": "🙀",
+    "polarity": -4
+  },
+  {
+    "emoji": "😴",
+    "polarity": 0
+  },
+  {
+    "emoji": "😪",
+    "polarity": 0
+  },
+  {
+    "emoji": "😄",
+    "polarity": 3
+  },
+  {
+    "emoji": "😸",
+    "polarity": 3
+  },
+  {
+    "emoji": "😃",
+    "polarity": 3
+  },
+  {
+    "emoji": "😺",
+    "polarity": 3
+  },
+  {
+    "emoji": "😈",
+    "polarity": -4
+  },
+  {
+    "emoji": "😏",
+    "polarity": 3
+  },
+  {
+    "emoji": "😼",
+    "polarity": 3
+  },
+  {
+    "emoji": "😭",
+    "polarity": -4
+  },
+  {
+    "emoji": "😛",
+    "polarity": 1
+  },
+  {
+    "emoji": "😝",
+    "polarity": 0
+  },
+  {
+    "emoji": "😜",
+    "polarity": -1
+  },
+  {
+    "emoji": "😎",
+    "polarity": 1
+  },
+  {
+    "emoji": "😓",
+    "polarity": -1
+  },
+  {
+    "emoji": "😅",
+    "polarity": 3
+  },
+  {
+    "emoji": "😫",
+    "polarity": -2
+  },
+  {
+    "emoji": "😤",
+    "polarity": 5
+  },
+  {
+    "emoji": "😒",
+    "polarity": -2
+  },
+  {
+    "emoji": "😩",
+    "polarity": -2
+  },
+  {
+    "emoji": "😉",
+    "polarity": 4
+  },
+  {
+    "emoji": "😟",
+    "polarity": -4
+  },
+  {
+    "emoji": "😋",
+    "polarity": 4
+  }
+]
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+/*
+ * Data.
+ */
+
+module.exports = require('./data/emoji-emotion.json');
+
+},{"./data/emoji-emotion.json":1}]},{},[2])(2)
+});
