83e9432a98aa6ed6629cb542b6709c9161854174
Titus Wormer <tituswormer@gmail.com>
2017-03-29 18:17:48 +0200

    Remove interpolation
    Scores in `emoji-emotion` run from 3 (most positive) to -4 (most
    negative).  Previously, you saw a score from 5 to -5 (just like in
    AFINN), as the build script interpolated those words to that scale.
    That however resulted in “holes” (such as `3` never appearing as a
    score).  Lastly, it meant that when the most positive score was
    removed (`:triumph` in c29abc5), all scores changed.
    
    I think this is bad, plus I’m planning on adding more emoji, and I’d
    rather have the scores be absolute and not changing if more emoji
    are added.

--- script/build-data.js
+++ script/build-data.js
@@ -4,8 +4,6 @@
 var fs = require('fs');
 var path = require('path');
 var gemoji = require('gemoji');
-var lerp = require('lerp');
-var unlerp = require('unlerp');
 var toJSON = require('plain-text-data-to-json');
 
 /* Read. */
@@ -16,24 +14,12 @@ faces = Object.keys(faces).sort().map(function (name) {
   return {
     name: name,
     emoji: gemoji.name[name].emoji,
-    polarity: faces[name]
+    polarity: Number(faces[name])
   };
 });
 
-/* Apply interpolation because emoticons represent more
- * emotion than words. */
-var min = Math.min.apply(Math, faces.map(pick));
-var max = Math.max.apply(Math, faces.map(pick));
-
 /* Write. */
-var doc = JSON.stringify(faces.map(function (face) {
-  var polarity = Math.round(lerp(-5, 5, unlerp(min, max, face.polarity)));
-  return {emoji: face.emoji, polarity: polarity};
-}), null, 2) + '\n';
+var doc = JSON.stringify(faces, null, 2) + '\n';
 
 /* Write the dictionary. */
 fs.writeFileSync(path.join('index.json'), doc);
-
-function pick(face) {
-  return face.polarity;
-}
